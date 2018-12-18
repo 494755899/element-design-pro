08038057b930dc6e83f93270f3f1834a8cb7e606
github_zhouzhixiang <494755899@qq.com>
2018-12-18 10:51:53 +0800

    home页面设计

--- src/components/customer/home.vue
+++ src/components/customer/home.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>首页</div>
+  <div class="element-design-pro-home">
+    <div class="element-design-pro-home-image">
+      <h1>Welcome Vue design Pro</h1>
+    </div>
+  </div>
 </template>
+
+<style lang="less" scoped>
+.element-design-pro-home {
+  margin-top: 50px;
+  .element-design-pro-home-image {
+    margin: 0 auto;
+    width: 1200px;
+    height: 603px;
+    background: url('../../assets/home.png')no-repeat;
+    background-size: 100% 100%;
+  }
+  h1 {
+    text-align: center;
+    padding-top: 400px;
+    font-size: 20px;
+    color: #FF8463;
+  }
+}
+</style>
